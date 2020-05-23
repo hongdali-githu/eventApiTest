a39b1de0572f735cbe7f6a13bf1c2cd1225b3329
wade <645821081@qq.com>
2020-05-23 21:20:33 +0800

    second version

--- event/testcase/run.py
+++
@@ -1,3 +0,0 @@
-#coding=utf-8
-import pytest
-pytest.main(["-s", "-v", "event_task_controller.py","-m", "demo"])
--- event/testcase/test_aa.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-import pytest
-from event.common.ExeclHandle import Execl_Handle_xlsx
-execl = Execl_Handle_xlsx(r"D:\事件感知\event\common\testdata.xlsx", 0)
-@pytest.mark.parametrize("case", execl.get_all())
-def test_aa(case):
-     print(case["params"])
-
